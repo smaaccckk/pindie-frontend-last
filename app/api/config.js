5e9178103c1bdf532bbb553fc2721574d17fd925
smaaccckk <168217834+smaaccckk@users.noreply.github.com>
2024-05-30 22:26:56 +0300

    какойто там комит уже непомню

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001/api";
+export const BASE_URL = "pindie-backend-last.nomorepartiesco.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
